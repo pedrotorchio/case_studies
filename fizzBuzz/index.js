68721c7eb5eb617bf64d02b0cd4056359878c2a1
pedro torchio <pedro@frankiefinancial.com>
2021-06-02 12:40:50 +1000

    adding new test

--- fizzBuzz/index.js
+++ fizzBuzz/index.js
@@ -21,7 +21,7 @@ const expect = require("expect");
  * @param  number num
  * @return string
  */
-expect(FizzBuzz(16)).toEqual("1 2 Fizz 4 Buzz Fizz 7 8");
+expect(FizzBuzz(8)).toEqual("1 2 Fizz 4 Buzz Fizz 7 8");
 expect(FizzBuzz(16)).toEqual("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16");
 function FizzBuzz(num) {
   
